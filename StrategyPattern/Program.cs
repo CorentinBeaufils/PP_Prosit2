8c29e25572e7eb5c9c23698935333b4413367420
CorentinBeaufils <corentin.beaufils@viacesi.fr>
2023-11-26 12:42:16 +0100

    Add project files.

---
+++ StrategyPattern/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using StrategyPattern;
+
+Calculator calculatrice = new Calculator(new AdditionStrategy());
+
+int res1 = calculatrice.DoOperation(1, 2);
+Console.WriteLine($"resultat de l'op : {res1}");
+
+calculatrice.SetStrategy(new SubstractStrategy());
+res1 = calculatrice.DoOperation(1, 4);
+
+Console.WriteLine($"resultat de l'op : {res1}");
---
+++ StrategyPattern/StrategyP.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyPattern
+{
+    public interface IOperationStrategy
+    {
+        int Execute(int a, int b);
+    }
+
+    public class AdditionStrategy : IOperationStrategy
+    {
+        public int Execute(int a, int b) { return a + b; }
+    }
+
+    public class SubstractStrategy : IOperationStrategy {
+        public int Execute(int a, int b) { return a- b; }
+    }
+
+    public class MultiplicationStrategy : IOperationStrategy
+    {
+        public int Execute(int a ,  int b) { return a*b; }
+    }
+
+
+    public class Calculator
+    {
+        private IOperationStrategy strategy;
+        public Calculator(IOperationStrategy strategy)
+        {
+            this.strategy = strategy;
+        }
+
+        public void SetStrategy(IOperationStrategy operationStrategy)
+        {
+            strategy = operationStrategy;
+        }
+
+        public int DoOperation(int a,int b) { 
+            return strategy.Execute(a,b);
+        }
+    }
+}
